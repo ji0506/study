841de407ee8880d3e0cd28d4cdad45e9d2348519
jiwan <audwldhks0@gmail.com>
2023-04-13 11:56:55 +0900

    if문기초

---
+++ src/study0412/ST01.java
@@ -0,0 +1,20 @@
+package study0412;
+import java.util.Scanner;
+
+public class ST01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("정수값 : ");
+		int n = stdIn.nextInt();
+		
+		
+		if(n < 0)
+		{
+			System.out.println("이 값은 음의 값입니다");
+		} 
+	}
+
+}
