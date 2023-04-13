b3acb2761bb93841569522b3af3287b50dccf441
jiwan <audwldhks0@gmail.com>
2023-04-13 11:58:32 +0900

    if문과else if문 활용

---
+++ src/study0412/ST05.java
@@ -0,0 +1,27 @@
+package study0412;
+
+import java.util.Scanner;
+
+public class ST05 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("정수값 : ");
+		int n = stdIn.nextInt();
+		
+		
+		if(n > 0)
+		{
+			System.out.println("이 값은 양수 값입니다");
+		}
+		else if(n < 0)
+			System.out.println("이 값은 음수 값입니다");
+		
+		else 
+			System.out.println("이 값은 0입니다");
+
+		
+	}
+
+}
