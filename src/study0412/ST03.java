a25625e51464156c6865f66e2511cee7f46711a8
jiwan <audwldhks0@gmail.com>
2023-04-13 11:57:45 +0900

    if을 통한%계산

---
+++ src/study0412/ST03.java
@@ -0,0 +1,25 @@
+package study0412;
+import java.util.Scanner;
+
+public class ST03 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("변수 A : "); 
+		int a = stdIn.nextInt();
+
+		System.out.println("변수 B : ");
+		int b = stdIn.nextInt();
+		
+		
+		if(a%b == 0)
+		{
+			System.out.println("B는 A의 약수 입니다");
+		}
+		else 
+			System.out.println("B는 A의 약수 아닙니다");
+
+		
+	}
+}
